2c5c80c85dbff776564f10e288f2ed4dda96978e
Livan.pro <me@livan.pro>
2024-11-24 16:09:46 +0000

    chore: fix lints

--- .prettierrc.js
+++ .prettierrc.js
@@ -8,15 +8,7 @@ const config = {
 	semi: false,
 	trailingComma: "all",
 	plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-packagejson"],
-	importOrder: [
-		"<BUILTIN_MODULES>",
-		"",
-		"<THIRD_PARTY_MODULES>",
-		"",
-		"^../(.*)$",
-		"",
-		"^[./]",
-	],
+	importOrder: ["<BUILTIN_MODULES>", "", "<THIRD_PARTY_MODULES>", "", "^../(.*)$", "", "^[./]"],
 }
 
 export default config
